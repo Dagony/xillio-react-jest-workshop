46601767a49b7bf9a3d7b3d002216b9e927f9202
Mark de Wit <mark.dewit@xillio.com>
2019-08-26 15:52:24 +0200

    Finish mocking a function with date

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 
 class Success extends Component {
     render() {
         return (
             <div>
                 <h1 className="ui centered">Details Successfully Saved</h1>
-                <span>Time: {moment().format("MMM Do YYY hh:mm:ss.sss a")}</span>
+                <span>Time: {Date.now()}</span>
             </div>
         );
     }
--- src/components/Success.test.js
+++ src/components/Success.test.js
@@ -3,6 +3,7 @@ import Success from './Success';
 import {shallow} from 'enzyme';
 
 describe('Success', () => {
+
     it('renders', () => {
         const wrapper = shallow(<Success />);
         // expect(wrapper.find('div')).toHaveLength(1);
@@ -13,5 +14,22 @@ describe('Success', () => {
     it('snapshot test fails if content is not equal to previous run', () => {
         const wrapper = shallow(<Success />);
         expect(wrapper.text()).toMatchSnapshot();
+    });
+
+    describe('with mocked date', () => {
+        let dateNowSpy;
+
+        beforeEach(() => {
+            dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => new Date('1984-10-06T02:20:00').getTime());
+        });
+
+        afterEach(() => {
+            dateNowSpy.mockRestore();
+        });
+
+        it('snapshot test no longer fails if content is mocked, so equal to previous run', () => {
+            const wrapper = shallow(<Success />);
+            expect(wrapper.text()).toMatchSnapshot();
+        });
     });
 });
