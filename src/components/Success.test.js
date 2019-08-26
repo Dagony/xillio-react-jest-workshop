833758505608859731d0619cbd67aaea3c820b4e
Mark de Wit <mark.dewit@xillio.com>
2019-08-26 13:08:53 +0200

    Finish tests

---
+++ src/components/Success.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Success from './Success';
+import {shallow} from 'enzyme';
+
+describe('Success', () => {
+    it('renders', () => {
+        const wrapper = shallow(<Success />);
+        expect(wrapper.find('div')).toHaveLength(1);
+        expect(wrapper.find('div').text()).toEqual('Details Successfully Saved');
+    });
+});
