bec26ce503be6744bdefe505a9f68b506f91a690
Mark de Wit <mark.dewit@xillio.com>
2019-08-26 13:23:38 +0200

    First snapshot test

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Success extends Component {
     render() {
         return (
             <div>
                 <h1 className="ui centered">Details Successfully Saved</h1>
+                <span>Time: {moment().format("MMM Do YYY hh:mm:ss.sss a")}</span>
             </div>
         );
     }
--- src/components/Success.test.js
+++ src/components/Success.test.js
@@ -5,7 +5,8 @@ import {shallow} from 'enzyme';
 describe('Success', () => {
     it('renders', () => {
         const wrapper = shallow(<Success />);
-        expect(wrapper.find('div')).toHaveLength(1);
-        expect(wrapper.find('div').text()).toEqual('Details Successfully Saved');
+        // expect(wrapper.find('div')).toHaveLength(1);
+        // expect(wrapper.find('div').text()).toEqual('Details Successfully Saved');
+        expect(wrapper.text()).toMatchSnapshot();
     });
 });
